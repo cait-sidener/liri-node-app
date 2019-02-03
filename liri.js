fb8f7b7efaa432c45a962dc9e17475e2271acb82
Cait Sidener <cait.sidener@gmail.com>
2019-02-02 19:47:12 -0600

    setting up files

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
 require("dotenv").config();
+var moment = require('moment');require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
